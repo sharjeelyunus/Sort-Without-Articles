48a15f9b725d811591836d651b488433b8bc8ff0
sharjeelyunus <sharjeel924@gmail.com>
2020-10-26 18:13:02 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,1 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
